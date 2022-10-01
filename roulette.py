d0c164d0c0f7820d1e295c5dbd5523571c5da1c9
Geoffrey Dunn <geoff@warmage.com>
2022-10-01 13:55:17 +1000

    Select a random image from the folder

--- roulette.py
+++ roulette.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 from pptx import Presentation
+from glob import glob
+import random
 
 
 presentation = Presentation('template_presentation.pptx')
@@ -14,8 +16,12 @@ second_slide = presentation.slides.add_slide(blank_layout)
 first_slide.shapes.title.text = "Random topic title!"
 first_slide.placeholders[1].text = "Powerpoint Roulette"
 
+# Select a random image
+image_filenames = glob('images/*')
+image_filename = random.choice(image_filenames)
+
 # Will strecth or shrink the image to the slide size
-pic = second_slide.shapes.add_picture('images/flightteam.jpg', 0, 0,
+pic = second_slide.shapes.add_picture(image_filename, 0, 0,
                                       presentation.slide_width, presentation.slide_height)
 
 presentation.save("random_presentation.pptx")
