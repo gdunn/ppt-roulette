1f1f750a51e069ff8a98b170ea4a72cbe37c1846
Geoffrey Dunn <geoff@warmage.com>
2022-10-01 12:13:23 +1000

    Start from an empty widescreen presentation

--- roulette.py
+++ roulette.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 from pptx import Presentation
+from pptx.util import Inches
 
 
-presentation = Presentation()
+presentation = Presentation('template_presentation.pptx')
 
 title_layout = presentation.slide_layouts[0]
 blank_layout = presentation.slide_layouts[6]
@@ -14,4 +15,4 @@ second_slide = presentation.slides.add_slide(blank_layout)
 first_slide.shapes.title.text = "Random topic title!"
 first_slide.placeholders[1].text = "Powerpoint Roulette"
 
-presentation.save("First_presentation.pptx")
+presentation.save("random_presentation.pptx")
